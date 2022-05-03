7dd6120b191bb84a0487699e2650be6feceade96
Mahabub Sunny <mahabubsany2@gmail.com>
2022-05-03 22:24:33 +0600

    Done

--- dist/server.js
+++ dist/server.js
@@ -3,5 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("./Config/index"));
-console.log(index_1.default.PORT);
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res, next) => {
+    res.send("Hello");
+});
+const server = app.listen(3000, () => console.log("Server running on 3000"));
